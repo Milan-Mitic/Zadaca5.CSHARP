74f51f7c922d7ff84277e691e35af66e3c4bb07c
Milan Mitic <62287697+Milan-Mitic@users.noreply.github.com>
2023-06-16 17:55:23 +0200

    Add files via upload

---
+++ Task1/Book.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Isbn { get; set; }
+        public int PublishYear { get; set; }
+        public int NumberOfPages { get; set; }
+
+        public void GetBookDetails()
+        {
+            Console.WriteLine($"The book's title is {Title}. Author is {Author}. International book's number is {Isbn}. The book published {PublishYear} year. The book has {NumberOfPages} pages.");
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book();
+            Console.WriteLine("Enter title of the book: ");
+            book.Title = Console.ReadLine();
+            Console.WriteLine("Enter author of the book: ");
+            book.Author = Console.ReadLine();
+            Console.WriteLine("Enter isbn number: ");
+            book.Isbn = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter year of publishing: ");
+            book.PublishYear = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter number of pages: ");
+            book.NumberOfPages = int.Parse(Console.ReadLine());
+
+            book.GetBookDetails();
+        }
+    }
+}
