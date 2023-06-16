aef588c7de2f71e5468e42ce64aebc3cce9e1a3b
Milan Mitic <62287697+Milan-Mitic@users.noreply.github.com>
2023-06-16 17:57:42 +0200

    Add files via upload

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            Console.WriteLine("Enter first name: ");
+            student.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter last name: ");
+            student.LastName = Console.ReadLine();
+            Console.WriteLine("Enter birthday (dd/mm/yyyy)");
+            student.Birthday = Convert.ToDateTime(Console.ReadLine());
+            Console.WriteLine("Enter gender: ");
+            student.Gender = Console.ReadLine();
+
+            Console.WriteLine(student.GetStudentDetails());
+
+        }
+    }
+}
---
+++ Task2/Student.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Gender { get; set; }
+
+        public Student()
+        {
+
+        }
+
+        public Student(string firstName, string lastName, DateTime birthday, string gender)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Birthday = birthday;
+            Gender = gender;
+        }
+
+        public int CalculateAge()
+        {
+            DateTime currentDate = DateTime.Now;    
+            int age = currentDate.Year - Birthday.Year;
+
+            if (currentDate < Birthday.AddYears(age))
+            {
+                age--;
+            }
+            return age;
+        }
+
+        public string GetStudentDetails()
+        {
+            return $"Name: {FirstName} {LastName}\nBirthday: {Birthday}\nGender: {Gender}\nAge: {CalculateAge()}";
+        }
+    }
+}
